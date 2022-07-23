b6fdeda2e26d171a34c91f38781bb1af14bf6a69
AhmedEssam61 <ahmed.essam@smartware.ae>
2022-07-23 04:07:19 +0200

    first

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit{
+  constructor(public auth: AuthService, private _HttpClient:HttpClient) {}
+
+  ngOnInit(): void {
+    this.auth.getAccessTokenSilently().subscribe(token => {
+      let httpHeaders = new HttpHeaders();
+      httpHeaders.set("Authorization", "Bearer AAAAAAAAAAAAAAAAAAAAACeBfAEAAAAAOeGYthgEL1mOaP5Qcw3fKbzDYhI%3DVIOTnxwHGXZLEz5cDmU2vZESv8kH2ZChXbRYJ9sjLfWg1Efad6");
+
+      this._HttpClient.get("https://api.twitter.com/2/tweets/search/recent?query=saw", {
+
+        headers: httpHeaders
+
+      }).subscribe(res => {
+        console.log(res);
+      });
+    });
+  }
+}
